b0cb54798a384dc419842e279d543f3675152ba2
Willy Prado <willyprado@MacBook-Pro-de-Willy.local>
2026-03-02 11:23:57 +0100

    Agregando proyectos

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,137 +1,136 @@
-import vlrstats from "../assets/images/projects/vlrstats/vlrstats.webp";
-import orlandomm from "../assets/images/projects/orlandomm/orlandomm.webp";
-import orlandommv2 from "../assets/images/projects/orlandommv2/orlandommv2.webp";
-import todolist from "../assets/images/projects/todolist/todolist.webp";
-import taply from "../assets/images/projects/taply/taply.webp";
-import vfa from "../assets/images/projects/vfa/vfa.webp";
-import triunvirato from "../assets/images/projects/triunvirato/triunvirato.webp";
-import vlr from "../assets/images/projects/vlr/vlr.webp";
+import lineaviva from "../assets/images/projects/lineaviva/lineaviva.png";
+import rickandmorty from "../assets/images/projects/jump2digital/rick_and_morty.png";
+import kibus from "../assets/images/projects/kibus/kibus.png";
+import udefine from "../assets/images/projects/udefine/udefine.png";
+import website_personal from "../assets/images/projects/website_personal/website_personal_v1.png";
+import pucelamatch from "../assets/images/projects/pucelamatch/pucela_match.png";
 
 export const projects = [
   {
     id: 1,
-    url: "triunvirato-bot",
+    url: "website_personal_v1.0",
     images: {
-      main: triunvirato,
+      main: website_personal,
     },
-    technologies: ["NodeJS", "DiscordJS"],
-    category: "experiments",
+    technologies: ["Javascript", "CSS", "HTML"],
+    category: "personal",
     es: {
-      title: "Triunvirato Bot",
+      title: "Website Personal V1.0",
       description:
-        'Este proyecto, que utiliza NodeJS y DiscordJS, es un bot de Discord que creé para mi grupo de amigos. El bot tiene varias características, como la capacidad de eliminar mensajes, mostrar memes aleatorios y bloquear canales de voz. Además, puede habilitar la función "Watch Together" y proporcionar ayuda cuando sea necesario. Este proyecto demuestra mis habilidades en desarrollo backend y trabajo con plataformas de terceros.',
+        'Mi sitio web personal lo construí desde cero bajo la guía de un tutor. Fue el proyecto final del bootcamp "Frontend Bootstrapping" en Medellín, Colombia. Utilicé JavaScript básico, HTML y CSS para crear este sitio web, que me permitió aprender los fundamentos del desarrollo web y sentar las bases para futuros proyectos. Este proyecto demuestra mi capacidad para aprender y aplicar nuevas habilidades en el desarrollo web.',
     },
     en: {
-      title: "Triunvirato Bot",
+      title: "Website Personal V1.0",
       description:
-        'This project, which uses NodeJS and DiscordJS, is a Discord bot that I created for my group of friends. The bot has various features such as the ability to delete messages, display random memes, and block voice channels. Additionally, it can enable a "Watch Together" feature and provide help when needed. This project showcases my skills in backend development and working with third-party platforms.',
+        'My personal website was built from scratch under the guidance of a tutor. It was the final project of the "Frontend Bootstrapping" bootcamp in Medellín, Colombia. I used basic JavaScript, HTML, and CSS to create this website, which allowed me to learn the fundamentals of web development and lay the groundwork for future projects. This project demonstrates my ability to learn and apply new skills in web development.',
     },
-    github: "https://github.com/Orloxx23/triunvirato-bot",
-    website: "",
+    github: "https://github.com/willyprados/willyprados.github.io",
+    website: "https://willyprados.github.io",
   },
   {
     id: 2,
-    url: "vfa",
+    url: "udefine",
     images: {
-      main: vfa,
+      main: udefine,
     },
-    technologies: ["ReactJS", "Youtube API"],
-    category: "experiments",
+    technologies: ["Astro", "Tailwind CSS"],
+    category: "recommended",
     es: {
-      title: "Valorant para todos",
+      title: "uDefine",
       description:
-        "Este proyecto web sencillo, creado con ReactJS y la API de YouTube, recopila una colección de videos para ayudar a los jugadores de Valorant a mejorar su juego. El sitio web permite a los usuarios acceder fácilmente a una variedad de videos instructivos y tutoriales relacionados con el juego. Este proyecto demuestra mis habilidades en desarrollo frontend, integración de API y diseño UX/UI.",
+        "Creado como parte del proyecto de grado de la formación profesional. Este proyecto lo desarrolle desde cero. Creé el diseño en Figma, implementé estilos con Tailwind y utilicé Astro como framework. El diseño de la página lo inspire en la combinación de colores de la Marca SAP. Implementé el proyecto en Cloudflare.",
     },
     en: {
-      title: "Valorant For all",
+      title: "uDefine",
       description:
-        "This simple web project, created using ReactJS and the YouTube API, compiles a collection of videos to help Valorant players improve their gameplay. The website allows users to easily access a variety of instructional videos and tutorials related to the game. This project showcases my skills in front-end development, API integration, and UX/UI design.",
+        "Created as part of the graduation project of the professional training. This project was developed from scratch. I created the design in Figma, implemented styles with Tailwind, and used Astro as the framework. The design of the page was inspired by the color scheme of the SAP Brand. I deployed the project on Cloudflare.",
     },
-    github: "https://github.com/Orloxx23/valorantfa-client",
-    website: "https://valorantfa-client.vercel.app",
+    github: "https://github.com/willyprados/uDefine-Astro",
+    website: "https://udefine-astro.pages.dev/en/",
   },
   {
     id: 4,
-    url: "todo-list",
+    url: "rickandmorty",
     images: {
-      main: todolist,
+      main: rickandmorty,
     },
-    technologies: ["ReactJS"],
+    technologies: ["JavaScript", "CSS", "HTML"],
     category: "practice",
     es: {
-      title: "Lista de tareas",
+      title: "Rik and Morty",
       description:
-        "Esta es una aplicación web de lista de tareas que creé mientras aprendía ReactJS en Platzi. Este proyecto demuestra mis habilidades en desarrollo front-end.",
+        "Creado como parte del Hackathon 'Jump2Digital', este proyecto lo desarrolle desde cero. Creé la estructura HTML, implementé estilos CSS y utilicé JavaScript. El diseño de la página lo inspire en la combinación de colores del sitio web de la api. Accedí a datos de personajes de la API de Rick and Morty a través de JavaScript e implementé el proyecto en Cloudflare. Este proyecto demuestra mis habilidades en desarrollo frontend, integración de API y despliegue en la nube.",
     },
     en: {
-      title: "Todo List",
+      title: "Rick and Morty",
       description:
-        "This is a task list web application that I created while learning ReactJS on Platzi. This project showcases my skills in front-end development.",
+        "Created as part of the 'Jump2Digital' Hackathon, this project was developed from scratch. I handled the HTML structure, implemented pure CSS styles, and utilized JavaScript. The design of the page was inspired by the color scheme of the parent website. I accessed character data from the Rick and Morty API through JavaScript and deployed the project on Netlify. This project demonstrates my skills in front-end development, API integration, and cloud deployment.",
     },
-    github: "https://github.com/Orloxx23/todolist",
-    website: "https://orloxx23.github.io/todolist/",
+    github: "https://github.com/willyprados/jump2digital",
+    website: "https://jump2digital.pages.dev/",
   },
   {
     id: 6,
-    url: "vlrstats",
+    url: "lineaviva",
     images: {
-      main: vlrstats,
+      main: lineaviva,
     },
-    technologies: ["ReactJS", "Firebase"],
-    category: "personal",
+    technologies: ["JavaScript", "CSS", "HTML"],
+    category: "experiments",
     es: {
-      title: "VLR Stats",
+      title: "LíneaVIVA",
       description:
-        "Esta aplicación web está diseñada para jugadores de Valorant que deseen ver su historial de partidas, saber con quién jugaron en cada una de ellas, hacer seguimiento a su progreso en rangos, estar al tanto de las últimas noticias del juego y acceder a una base de datos de retículas creadas por la comunidad. Este proyecto demuestra mis habilidades tanto en desarrollo front-end como en back-end, y en la integración de API.",
+        "Línea Viva es un proyecto de revista digital desarrollado con el objetivo de difundir contenido corporativo y editorial de forma accesible y atractiva. El sitio fue diseñado y maquetado íntegramente con HTML, CSS y algo de JavaScript, asegurando un rendimiento óptimo y una experiencia de usuario clara. Uno de los requisitos técnicos principales fue garantizar la compatibilidad con navegadores antiguos, incluyendo Internet Explorer 7, lo que implicó aplicar técnicas de maquetación y scripts adaptados a estándares heredados. La revista se publica y promociona mensualmente a través del portal oficial de ISAGEN, sirviendo como canal digital informativo para sus usuarios y colaboradores.",
     },
     en: {
-      title: "VLR Stats",
+      title: "LíneaVIVA",
       description:
-        "This web application is designed for Valorant players to view their match history, see who they played with in each game, track their progress in ranks, keep up with the latest game news, and access a database of community-created crosshairs. This project showcases my skills in front-end development, back-end development, and API integration.",
+        "Línea Viva is a digital magazine project developed with the aim of disseminating corporate and editorial content in an accessible and attractive way. The site was designed and coded entirely with HTML, CSS, and some JavaScript, ensuring optimal performance and a clear user experience. One of the main technical requirements was to ensure compatibility with older browsers, including Internet Explorer 7, which involved applying layout techniques and scripts adapted to legacy standards. The magazine is published and promoted monthly through ISAGEN's official portal, serving as an informative digital channel for its users and collaborators.",
     },
-    github: "https://github.com/Orloxx23/vlrstats",
-    website: "https://vlrstats.vercel.app",
+    github: "https://github.com/willyprados/ISAGEN-amazine-133",
+    website:
+      "https://www.isagen.com.co/LineaVIVA/lineaviva_edicion_133/html/page-01.html",
   },
   {
     id: 7,
-    url: "taply",
+    url: "kibus",
     images: {
-      main: taply,
+      main: kibus,
     },
-    technologies: ["React Native", "NodeJS", "ElectronJS"],
-    category: "recommended",
+    technologies: ["React.js", "Style Components", "Vite"],
+    category: "practice",
     es: {
-      title: "Taply",
+      title: "Kibus",
       description:
-        "Este proyecto de codigo abierto consta de una aplicación de escritorio con un servidor Node y una aplicación móvil que permite a los jugadores de Valorant controlar el juego desde su dispositivo móvil. Con esta aplicación, los jugadores pueden seleccionar su agente y administrar su sala de juego, incluyendo la capacidad de cambiar el modo de juego. Este proyecto demuestra mis habilidades en desarrollo full-stack, desarrollo móvil e integración de API.",
+        "Este proyecto sirvió como prueba técnica para la empresa Rocket Digital. Fue construido usando Vite, React.js y Style Components.",
     },
     en: {
-      title: "Taply",
+      title: "Kibus",
       description:
-        "This open-source project consists of a desktop application with a Node server and a mobile app that allows Valorant players to control the game from their mobile device. With this app, players can select their agent and manage their game room, including changing the game mode. This project showcases my skills in full-stack development, mobile development, and API integration.",
+        "This project served as a technical test for the company Rocket Digital. It was built using Vite, React.js, and Style Components.",
     },
-    github: "",
-    website: "https://taply.click",
+    github: "https://github.com/willyprados/kibus-prueba",
+    website: "https://kibus.netlify.app",
   },
   {
     id: 8,
-    url: "vlrggscraper",
+    url: "pucelamatch",
     images: {
-      main: vlr,
+      main: pucelamatch,
     },
-    technologies: ["NextJS", "NodeJS"],
+    technologies: ["Javascript", "CSS", "HTML"],
     category: "experiments",
     es: {
-      title: "vlr.gg Scraper",
+      title: "Pucela Match",
       description:
-        "Este proyecto presenta una API que realiza scraping en vlr.gg, un popular sitio web de torneos de Valorant. La API permite a los usuarios obtener datos de torneos, estadísticas de jugadores y otra información de vlr.gg y utilizarla en sus aplicaciones. Se ha creado una página web dedicada para probar la funcionalidad de la API, y la propia API está alojada de forma pública para que cualquier persona pueda acceder a ella.",
+        "Pucela Match es una aplicación interactiva inspirada en la dinámica de Tinder, la desarrolle para el Ayuntamiento de Valladolid y presentada en un estand institucional durante un evento en Valladolid. Su propuesta convierte el aprendizaje en un juego: los participantes debían deslizar perfiles de personajes históricos y decidir si habían tenido o no relación con la ciudad. El objetivo era poner a prueba el conocimiento local de forma divertida, fomentando el interés por la historia y la cultura vallisoletana mediante una experiencia gamificada, intuitiva y social que invitaba a competir, descubrir curiosidades y aprender mientras se jugaba.",
     },
     en: {
-      title: "vlr.gg Scraper",
+      title: "Pucela Match",
       description:
-        "This project showcases an API that performs scraping on vlr.gg, a popular Valorant tournament website. The API allows users to retrieve tournament data, player statistics, and other information from vlr.gg and use it in their applications. A dedicated webpage is created to test the API's functionality, and the API itself is hosted publicly for anyone to access.",
+        "Pucela Match is an interactive application inspired by the dynamics of Tinder, developed for the City Council of Valladolid and presented at an institutional stand during an event in Valladolid. Its proposal turns learning into a game: participants had to swipe profiles of historical characters and decide whether they had a relationship with the city or not. The goal was to test local knowledge in a fun way, fostering interest in the history and culture of Valladolid through a gamified, intuitive, and social experience that invited competition, discovery of curiosities, and learning while playing.",
     },
-    github: "https://github.com/Orloxx23/vlrscraper-docs",
-    website: "https://vlrggapi-docs.vercel.app",
+    github: "https://github.com/willyprados/puselamatch",
+    website: "https://pucelamatch.pages.dev/",
   },
 ];
